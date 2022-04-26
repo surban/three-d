4cb31282ecd553c1d91ce5d93c8b0a1dfedfb05a
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-04-26 14:24:48 +0200

    Fix

--- src/window/headless.rs
+++ src/window/headless.rs
@@ -70,12 +70,6 @@ fn build_context<T1: ContextCurrentState>(
     use glutin::platform::unix::EventLoopExtUnix;
     let el = EventLoopExtUnix::new_any_thread();
 
-    /*println!("Trying surfaceless");
-    let err1 = match build_context_surfaceless(cb.clone(), &el) {
-        Ok(ctx) => return Ok((ctx, el)),
-        Err(err) => err,
-    };*/
-
     println!("Trying headless");
     let err2 = match build_context_headless(cb.clone(), &el) {
         Ok(ctx) => return Ok((ctx, el)),
@@ -88,7 +82,13 @@ fn build_context<T1: ContextCurrentState>(
         Err(err) => err,
     };
 
-    Err([err2, err3])
+    println!("Trying surfaceless");
+    let err1 = match build_context_surfaceless(cb.clone(), &el) {
+        Ok(ctx) => return Ok((ctx, el)),
+        Err(err) => err,
+    };
+
+    Err([err1, err2, err3])
 }
 
 #[cfg(not(target_os = "linux"))]
